b6562a7ca0ee448c1834fe0c0a64f82d2cdfd52c
a-wong-8 <awong88@berkeley.edu>
2023-04-21 15:32:53 -0700

    two_sum done

--- TDD/lib/problems.rb
+++ TDD/lib/problems.rb
@@ -8,4 +8,14 @@ class Array
         hash.keys
     end
 
+    def two_sum
+        final = []
+        self.each_with_index do |ele1, ind1|
+            self.each_with_index do |ele2, ind2|
+                final << [ind1, ind2] if ele1 + ele2 == 0 && ind2 > ind1 
+            end
+        end
+        final
+    end
+
 end
--- TDD/spec/problems_spec.rb
+++ TDD/spec/problems_spec.rb
@@ -6,4 +6,15 @@ describe Array do
         expect([1,2,1,3,3].uniq). to eq([1,2,3])
         end
     end
+
+    describe "two_sum" do 
+        it "finds all pairs of positions where the elements at those positions sum to zero" do 
+            expect([-1, 0, 2, -2, 1].two_sum). to eq([[0, 4], [2, 3]])
+        end
+        it "it returns the smaller index before the larger index" do
+            expect([-1, 0, 1].two_sum).to_not eq([[2, 0]])
+        
+    end
+    end
+
 end
